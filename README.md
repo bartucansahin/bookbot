32012985bea0554bc54105992e82928fa834ce87
bartucansahin <bartucan.dev@gmail.com>
2024-02-27 17:14:26 +0300

    Created some functions to be able to read the file, count the words and letters, and  generating a report on a sorted, organized way

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-Bookbot is my first git project, It is sometimes hard and frustrating to learn how to code, but I learned a lot at 27 years of age.
-Even though all the odds are against me right now, I will overcome the obstacles. I will have a nice life in the end. 
-A life worth living for and I will make the lives around me better. 
-It's never too old to dream a new dream. - C. S. 
+#This script is meant to take input books as text files and give the output of a report on how many words have been written in it with additional information of letter usage. 
--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 def main():
     path_to_file = "books/frankenstein.txt"
-    try:
-        with open(path_to_file) as f:
-            file_contents = f.read()
-            print(file_contents)
-    except FileNotFoundError:
-        print("File not found.")
-    except PermissionError:
-        print("Permission denied.")
-    except UnicodeDecodeError:
-        print("File is not encoded in a way that Python can read.")
+    text = get_book_text(path_to_file)
+    num_words = count_words(text)
+    letter_list = count_letters(text)
+    report = generate_report(letter_list)
     
+    return report
+    
+def count_words(text):
+    word_counter = 0
+    words = text.split()
+    for word in words:
+        word_counter += 1
+    return word_counter
+
+def count_letters(text):
+    letter_list = []
+    text = text.lower()
+    for i in text:
+        found = False
+        if i.isalpha(): 
+            for item in letter_list:
+                if item['letter'] == i:
+                    item['count'] += 1
+                    found = True
+                    break
+            if not found:
+                letter_list.append({'letter': i, 'count': 1})
+    return letter_list
     
 
+def generate_report(letter_list):
+    letter_list.sort(reverse=True, key=lambda x: x['count'])
+    for item in letter_list:
+        print(f"The {item['letter']} was written {item['count']} times")
+
+def sort_on(dict):
+    return dict["count"]
+
+def get_book_text(path):
+    with open("books/frankenstein.txt") as f:
+        return f.read()
+
 main()
+
 
 
